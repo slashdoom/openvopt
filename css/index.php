1177e0a37524a18468f5034f9aaf5b263f8399dc
Slashdoom <slashdoom@users.noreply.github.com>
2015-05-12 22:20:37 -0400

    Update index.php

--- css/index.php
+++ css/index.php
@@ -1,24 +1,4 @@
-<!DOCTYPE html>
-<html>
-
-  <head>
-
-    <title>OpenVoPT: Redirect</title>
-
-  </head>
-
-  <body>
-
 <?php
-
-  $root = realpath($_SERVER["DOCUMENT_ROOT"]);
-
-  include($root."/include/config.inc.php");
-
-  echo '<meta http-equiv="refresh" content ="0; url=http://'.$url.'">'."\n"
-
+  header("Location: ../login.php");
+  die();
 ?>
-
-  </body>
-
-</html>
