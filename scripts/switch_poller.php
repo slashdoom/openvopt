2ccb7c01a34384c44c248e3b9bd9a90f68358f7b
Slashdoom <slashdoom@users.noreply.github.com>
2015-07-16 13:16:42 -0400

    Update switch_poller.php

--- scripts/switch_poller.php
+++ scripts/switch_poller.php
@@ -45,11 +45,11 @@
           foreach($host->getCDPCiscoPhones() as $phone) {
 
             // trim SQL results
-            $sphone            = '"'.preg_replace('~[\r\n]+~', '', $phone['phone']).'"';
-            $sswitch           = '"'.preg_replace('~[\r\n]+~', '', $phone['switch']).'"';
-            $sswitch_loc       = '"'.preg_replace('~[\r\n]+~', '', $phone['switch_loc']).'"';
-            $sswitch_int       = '"'.preg_replace('~[\r\n]+~', '', $phone['switch_int']).'"';
-            $sswitch_int_alias = '"'.preg_replace('~[\r\n]+~', '', $phone['switch_int_alias']).'"';
+            $sphone            = preg_replace('~[\r\n]+~', '', $phone['phone']);
+            $sswitch           = preg_replace('~[\r\n]+~', '', $phone['switch']);
+            $sswitch_loc       = preg_replace('~[\r\n]+~', '', $phone['switch_loc']);
+            $sswitch_int       = preg_replace('~[\r\n]+~', '', $phone['switch_int']);
+            $sswitch_int_alias = preg_replace('~[\r\n]+~', '', $phone['switch_int_alias']);
             // insert phone info into tracking table
             $insert_tracking="INSERT INTO tracking (phone,switch,switch_loc,switch_int,switch_int_alias) 
                               VALUES('".$sphone."','"
